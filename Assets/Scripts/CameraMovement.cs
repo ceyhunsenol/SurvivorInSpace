59bf5e312bf528ae555a81683bcd088d770598e0
Gizem <113115962+giz08@users.noreply.github.com>
2023-06-11 19:53:13 -0400

    Terrain and CameraMovement.cs eklendi. Particle system eklendi

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 offset; // Kamera ile Character arasındaki mesafe
+    public float smootSpeed = 0.125f;
+    public Transform character;
+  
+
+    
+
+
+
+    // private void Start() => offset = transform.position - Character.position;
+    // Character ile kamera arasındaki mesafeyi kaydet
+
+    // private void LateUpdate() => transform.position = Character.position + offset;
+
+    private void LateUpdate()
+    {
+       transform.position = character.position + offset;
+        
+    }
+}
--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public static PlayerHealthController instance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance= this;
+    }
+    public float currentHealth, maxHealth;
+
+    public Slider healthSlider;
+
+     void Start()
+    {
+        currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        currentHealth -= damageToTake;
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        healthSlider.value = currentHealth;
     }
 }
